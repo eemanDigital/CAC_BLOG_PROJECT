0eef6f8f13e55ad6a0b33575e15754e5f3e61dfb
eemanDigital <eemandigitalconcept@gamil.com>
2022-08-03 11:00:03 +0100

    new changes made

--- src/component/Aside.jsx
+++ src/component/Aside.jsx
@@ -7,9 +7,9 @@ function Aside() {
     <div className='aside'>
       <div className="asideText">
     <p className=" border animate__animated animate__zoomIn">Top Digital Agent</p>
-    <h1   className="animate__animated animate__lightSpeedInLeft">we deliver with expertise and dilligence</h1>
+    <h1   className="animate__animated animate__lightSpeedInLeft">register your company/business/ngo with us</h1>
 
-<p className="animate__animated animate__zoomIn">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit debitis quo velit magni veniam temporibus facere quaerat incidunt eaque id!</p>
+<p className="animate__animated animate__zoomIn">we deliver with expertise and dilligence</p>
       </div>
 
     </div>
